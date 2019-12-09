5c2da7fd2c2f6af50dc529f514f3b4f862a83f4d
Brett Law <brett.law@samsungnext.com>
2019-12-09 01:48:11 -0800

    day 9 solutions

---
+++ day9.js
@@ -0,0 +1,186 @@
+let program = [1102,34463338,34463338,63,1007,63,34463338,63,1005,63,53,1101,3,0,1000,109,988,209,12,9,1000,209,6,209,3,203,0,1008,1000,1,63,1005,63,65,1008,1000,2,63,1005,63,904,1008,1000,0,63,1005,63,58,4,25,104,0,99,4,0,104,0,99,4,17,104,0,99,0,0,1102,1,31,1018,1102,352,1,1023,1101,0,1,1021,1101,0,33,1003,1102,1,36,1007,1102,21,1,1005,1101,359,0,1022,1101,0,787,1024,1102,1,24,1011,1101,30,0,1014,1101,22,0,1016,1101,0,0,1020,1102,1,29,1000,1101,778,0,1025,1102,23,1,1017,1102,1,28,1002,1101,38,0,1019,1102,1,27,1013,1102,1,32,1012,1101,0,37,1006,1101,444,0,1027,1102,1,20,1009,1101,0,447,1026,1101,0,39,1008,1101,35,0,1010,1102,559,1,1028,1102,26,1,1004,1102,1,25,1015,1102,1,34,1001,1101,0,554,1029,109,-3,2101,0,9,63,1008,63,34,63,1005,63,205,1001,64,1,64,1105,1,207,4,187,1002,64,2,64,109,23,21107,40,39,-7,1005,1013,227,1001,64,1,64,1106,0,229,4,213,1002,64,2,64,109,-17,1202,-2,1,63,1008,63,36,63,1005,63,249,1106,0,255,4,235,1001,64,1,64,1002,64,2,64,109,-6,1202,10,1,63,1008,63,36,63,1005,63,277,4,261,1106,0,281,1001,64,1,64,1002,64,2,64,109,-2,1208,9,26,63,1005,63,303,4,287,1001,64,1,64,1106,0,303,1002,64,2,64,109,32,1206,-7,321,4,309,1001,64,1,64,1106,0,321,1002,64,2,64,109,-29,1207,7,20,63,1005,63,337,1105,1,343,4,327,1001,64,1,64,1002,64,2,64,109,27,2105,1,-2,1001,64,1,64,1106,0,361,4,349,1002,64,2,64,109,-25,2108,39,7,63,1005,63,377,1106,0,383,4,367,1001,64,1,64,1002,64,2,64,109,1,1201,6,0,63,1008,63,36,63,1005,63,409,4,389,1001,64,1,64,1105,1,409,1002,64,2,64,109,1,2102,1,1,63,1008,63,33,63,1005,63,435,4,415,1001,64,1,64,1105,1,435,1002,64,2,64,109,28,2106,0,-3,1106,0,453,4,441,1001,64,1,64,1002,64,2,64,109,-13,21101,41,0,1,1008,1018,44,63,1005,63,477,1001,64,1,64,1106,0,479,4,459,1002,64,2,64,109,4,21108,42,42,-2,1005,1019,501,4,485,1001,64,1,64,1106,0,501,1002,64,2,64,109,-21,2101,0,2,63,1008,63,28,63,1005,63,523,4,507,1105,1,527,1001,64,1,64,1002,64,2,64,109,26,1205,-5,545,4,533,1001,64,1,64,1105,1,545,1002,64,2,64,109,3,2106,0,-1,4,551,1106,0,563,1001,64,1,64,1002,64,2,64,109,-33,1201,4,0,63,1008,63,28,63,1005,63,583,1105,1,589,4,569,1001,64,1,64,1002,64,2,64,109,11,2107,27,-3,63,1005,63,609,1001,64,1,64,1106,0,611,4,595,1002,64,2,64,109,8,21102,43,1,3,1008,1018,43,63,1005,63,637,4,617,1001,64,1,64,1105,1,637,1002,64,2,64,109,-5,21108,44,41,0,1005,1010,653,1105,1,659,4,643,1001,64,1,64,1002,64,2,64,109,-13,2108,21,8,63,1005,63,681,4,665,1001,64,1,64,1106,0,681,1002,64,2,64,109,6,1207,0,34,63,1005,63,703,4,687,1001,64,1,64,1105,1,703,1002,64,2,64,109,7,1208,-7,35,63,1005,63,723,1001,64,1,64,1106,0,725,4,709,1002,64,2,64,109,-13,2102,1,7,63,1008,63,23,63,1005,63,745,1105,1,751,4,731,1001,64,1,64,1002,64,2,64,109,13,1205,10,767,1001,64,1,64,1105,1,769,4,757,1002,64,2,64,109,14,2105,1,0,4,775,1001,64,1,64,1106,0,787,1002,64,2,64,109,-20,21107,45,46,7,1005,1011,809,4,793,1001,64,1,64,1105,1,809,1002,64,2,64,109,-3,2107,25,3,63,1005,63,827,4,815,1106,0,831,1001,64,1,64,1002,64,2,64,109,13,1206,7,847,1001,64,1,64,1106,0,849,4,837,1002,64,2,64,109,-11,21101,46,0,7,1008,1010,46,63,1005,63,871,4,855,1106,0,875,1001,64,1,64,1002,64,2,64,109,15,21102,47,1,-4,1008,1014,48,63,1005,63,895,1106,0,901,4,881,1001,64,1,64,4,64,99,21102,27,1,1,21101,0,915,0,1106,0,922,21201,1,63208,1,204,1,99,109,3,1207,-2,3,63,1005,63,964,21201,-2,-1,1,21102,1,942,0,1106,0,922,21202,1,1,-1,21201,-2,-3,1,21101,957,0,0,1105,1,922,22201,1,-1,-2,1106,0,968,21201,-2,0,-2,109,-3,2106,0,0];
+//program = [104,1125899906842624,99];
+//program = [1102,34915192,34915192,7,4,7,99,0];
+//program = [109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99];
+
+const OP_ADD = 1;
+const OP_MULTIPLY = 2;
+const OP_INPUT = 3;
+const OP_OUTPUT = 4;
+const OP_JUMP_IF_TRUE = 5;
+const OP_JUMP_IF_FALSE = 6;
+const OP_LESS_THAN = 7;
+const OP_EQUALS = 8;
+const OP_RELBASE = 9;
+
+const OP_END = 99;
+
+let instructions = {};
+instructions[OP_ADD] = 'ADD';
+instructions[OP_MULTIPLY] = 'MULTIPLY';
+instructions[OP_INPUT] = 'INPUT';
+instructions[OP_OUTPUT] = 'OUTPUT';
+instructions[OP_JUMP_IF_TRUE] = 'JUMP_IF_TRUE';
+instructions[OP_JUMP_IF_FALSE] = 'JUMP_IF_FALSE';
+instructions[OP_LESS_THAN] = 'LESS_THAN';
+instructions[OP_EQUALS] = 'EQUALS';
+instructions[OP_END] = 'END';
+instructions[OP_RELBASE] = 'RELBASE';
+
+
+/**
+ * Now using a class as a "Computer" so we can instantiate multiple of them to run at once and maintain individual states of each
+ *
+ * @param inputProgram - program to initialize this computer with
+ * @param initialInputs - initial set of inputs for this computer
+ * @constructor
+ *
+ */
+function Computer(inputProgram, initialInputs) {
+
+   let inputQueue = (initialInputs || []).slice();
+
+   let memory = inputProgram.slice();
+
+   let p = 0;
+   let _isRunning = true;
+   let relbase = 0;
+
+   function getValue(b, mode) {
+      if (mode === 0) return memory[b] || 0;
+      if (mode === 1) return b || 0;
+      if (mode === 2) return memory[relbase + b] || 0;
+
+      return 0;
+   }
+
+   /**
+    * This function is overkill in parsing out parameters to be used later, but should allow easy debugging if needed
+    */
+   function processNextOpcode() {
+      let opcode = memory[p] % 100;
+      let instruction = instructions[opcode];
+
+      let op = {
+         instruction,
+         opcode,
+         p,
+         relbase,
+         b0: memory[p] || 0,
+         b1: memory[p + 1] || 0,
+         b2: memory[p + 2] || 0,
+         b3: memory[p + 3] || 0,
+      };
+
+      op.mode1 = Math.floor(op.b0/100) % 10;
+      op.mode2 = Math.floor(op.b0/1000) % 10;
+      op.mode3 = Math.floor(op.b0/10000) % 10;
+
+      op.m1output = (op.mode1 === 2 ? relbase + op.b1:op.b1);
+      op.m2output = (op.mode2 === 2 ? relbase + op.b2:op.b2);
+      op.m3output = (op.mode3 === 2 ? relbase + op.b3:op.b3);
+
+      op.v1 = getValue(op.b1, op.mode1);
+      op.v2 = getValue(op.b2, op.mode2);
+      op.v3 = getValue(op.b3, op.mode3);
+
+      return op;
+   }
+
+   function run(inputs, getFirstOutput) {
+
+      if (inputs) inputQueue = inputQueue.concat(inputs);
+
+      let LAST_OUTPUT = 0;
+
+      while (true) {
+         let op = processNextOpcode();
+
+         switch(op.opcode) {
+            case OP_ADD:
+               memory[op.m3output] = op.v1 + op.v2;
+               p += 4;
+               break;
+
+            case OP_MULTIPLY:
+               memory[op.m3output] = op.v1 * op.v2;
+               p += 4;
+               break;
+
+            case  OP_INPUT:
+               let inValue = inputQueue.shift();
+               memory[op.m1output] = inValue;
+               p += 2;
+               break;
+
+            case OP_OUTPUT:
+               LAST_OUTPUT = op.v1;
+               p += 2;
+               if (getFirstOutput) {
+                  console.log("OUTPUT:", LAST_OUTPUT);
+               }
+               break;
+
+            case OP_JUMP_IF_TRUE:
+               if (op.v1) {
+                  p = op.v2;
+               } else {
+                  p += 3;
+               }
+               break;
+
+            case OP_JUMP_IF_FALSE:
+               if (op.v1 === 0) {
+                  p = op.v2;
+               } else {
+                  p += 3;
+               }
+               break;
+
+            case OP_LESS_THAN:
+               memory[op.m3output] = (op.v1 < op.v2 ? 1 : 0);
+               p += 4;
+               break;
+
+            case OP_EQUALS:
+               memory[op.m3output] = (op.v1 === op.v2 ? 1:0);
+               p += 4;
+               break;
+
+            case OP_RELBASE:
+               relbase += op.v1;
+               p += 2;
+               break;
+
+            case OP_END:
+               _isRunning = false;
+               if (getFirstOutput) {
+                  return 0;
+               } else {
+                  return LAST_OUTPUT;
+               }
+         }
+      }
+   }
+
+   function isRunning() {
+      return _isRunning;
+   }
+
+   //
+   //  Publicly accessible functions...
+   //
+   this.run = run;
+   this.isRunning = isRunning;
+}
+
+
+let computer = new Computer(program, [1]);
+let output = computer.run(null, false);
+
+console.log("Result for Day 9, Part 1:", output);
+
+let computer2 = new Computer(program, [2]);
+let output2 = computer2.run(null, false);
+
+console.log("Result for Day 9, Part 2:", output2);
